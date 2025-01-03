1d158f5ce55608d1020af70d567564876f208731
Chen Li <1472424319@qq.com>
2025-01-03 20:56:52 +0800

    20250103

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("requirements.txt") as f:
 
 setup(
     name = "TFcomb",
-    version = "0.1",
+    version = "1.0",
     keywords = ["pip", "tfcomb"],
-    description = "TFcomb xxx",
-    long_description = "xxx",
+    description = "TFcomb is a python library to identify reprogramming TFs and TF combinations using scRNA-seq and scATAC-seq data.",
+    long_description = "TFcomb is a python library to identify reprogramming TFs and TF combinations using scRNA-seq and scATAC-seq data.",
     license = "MIT License",
     url = "https://github.com/Chen-Li-17/TFcomb",
     author = "Chen Li",
@@ -19,6 +19,7 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
